89e7716d791b1ac4f248bc5dbbc46baf182fea12
Daniel Sanche <d.sanche14@gmail.com>
2023-01-19 09:16:36 -0800

    removed prints

--- google/cloud/bigtable_async/row_merger.py
+++ google/cloud/bigtable_async/row_merger.py
@@ -20,19 +20,16 @@ from datetime import datetime
 
 from typing import Deque, Optional, List, Dict, Any
 
-import inspect
-# java implementation: 
+# java implementation:
 # https://github.com/googleapis/java-bigtable/blob/8b120de58f0dfba3573ab696fb0e5375e917a00e/google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/readrows/RowMerger.java
 
 class RowMerger():
 
     def __init__(self):
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         self.merged_rows:Deque[Row] = deque([])
         self.state_machine = StateMachine()
 
     def push(self, new_data:ReadRowsResponse):
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         last_scanned = new_data.last_scanned_row_key
         # if the server sends a scan heartbeat, notify the state machine.
         if last_scanned:
@@ -49,7 +46,6 @@ class RowMerger():
         """
         one or more rows are ready and waiting to be consumed
         """
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         return self.merged_rows
 
     def has_partial_frame(self) -> bool:
@@ -57,14 +53,12 @@ class RowMerger():
         Returns true if the merger still has ongoing state
         By the end of the process, there should be no partial state
         """
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         return self.has_full_frame() or self.state_machine.is_row_in_progress()
 
     def pop(self) -> Row:
         """
         Return a row out of the cache of waiting rows
         """
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         return self.merged_rows.popleft()
 
 
@@ -75,7 +69,6 @@ class StateMachine():
         self.reset()
 
     def reset(self):
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         self.current_state:Optional[str] = AWAITING_NEW_ROW(self)
         self.last_cell_data:Dict[str, Any] = {}
         # self.last_complete_row_key:Optional[bytes] = None
@@ -91,22 +84,18 @@ class StateMachine():
         self.adapter.reset()
 
     def handle_last_scanned_row(self, last_scanned_row_key:bytes):
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         pass
 
     def handle_chunk(self, chunk:ReadRowsResponse.CellChunk):
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         self.current_state = self.current_state.handle_chunk(chunk);
 
     def has_complete_row(self) -> bool:
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         return isinstance(self.current_state, AWAITING_ROW_CONSUME) and self.complete_row is not None
 
     def consume_row(self) -> Row:
         """
         Returns the last completed row and transitions to a new row
         """
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         if not self.has_complete_row() or self.complete_row is None:
             raise RuntimeError("No row to consume")
         row = self.complete_row
@@ -114,7 +103,6 @@ class StateMachine():
         return row
 
     def is_row_in_progress(self) -> bool:
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         return isinstance(self.current_state, AWAITING_NEW_ROW)
 
     def handle_commit_row(self) -> "State":
@@ -141,11 +129,9 @@ class State:
         self._owner = owner
 
     def handle_last_scanned_row(self, last_scanned_row_key:bytes) -> "State":
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         raise NotImplementedError
 
     def handle_chunk(self, chunk:ReadRowsResponse.CellChunk) -> "State":
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         raise NotImplementedError
 
 class AWAITING_NEW_ROW(State):
@@ -157,7 +143,6 @@ class AWAITING_NEW_ROW(State):
     """
 
     def handle_chunk(self, chunk:ReadRowsResponse.CellChunk) -> "State":
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         self._owner.adapter.start_row(chunk.row_key)
         # the first chunk signals both the start of a new row and the start of a new cell, so
         # force the chunk processing in the AWAITING_CELL_VALUE.
@@ -255,46 +240,38 @@ class RowBuilder():
 
     def reset(self) -> None:
         """called when the current in progress row should be dropped"""
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         self.current_key:Optional[bytes] = None
         self.working_cell:Optional[CellData] = None
         self.previous_cells:List[CellData] = []
 
     def create_scan_marker_row(self, key:bytes) -> Row:
         """creates a special row to mark server progress before any data is received"""
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         return Row(key)
 
     def start_row(self, key:bytes) -> None:
         """Called to start a new row. This will be called once per row"""
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         self.current_key = key
 
     def start_cell(self, family:str, qualifier:bytes, timestamp:int,labels:List[str], size:int) -> None:
         """called to start a new cell in a row."""
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         self.working_cell = CellData(family, qualifier, timestamp, labels, bytearray())
 
     def cell_value(self, value:bytes) -> None:
         """called multiple times per cell to concatenate the cell value"""
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         self.working_cell.value.extend(value)
 
     def finish_cell(self) -> None:
         """called once per cell to signal the end of the value (unless reset)"""
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         self.previous_cells.append(self.working_cell)
         self.working_cell = None
 
     def finish_row(self) -> Row:
         """called once per row to signal that all cells have been processed (unless reset)"""
-        print(f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}")
         new_row = DirectRow(self.current_key)
         for cell in self.previous_cells:
             # TODO: handle timezones?
             # should probably make a new row class
             timestamp = datetime.fromtimestamp(cell.timestamp/1e6)
-            print(cell)
             new_row.set_cell(cell.family, cell.qualifier, bytes(cell.value), timestamp)
         self.previous_cells.clear()
         self.current_key = None
