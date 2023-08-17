544c0d2cb73d437ff456e747af5bc1d8cbae8dc6
Daniel Sanche <d.sanche14@gmail.com>
2023-08-17 09:51:00 -0700

    silence kill line if process already ended

--- test_proxy/run_tests.sh
+++ test_proxy/run_tests.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 function finish {
-  kill -- -$$ # kill all child processes
+  kill -- -$$  2> /dev/null # kill all child processes
 }
 trap finish EXIT
 
